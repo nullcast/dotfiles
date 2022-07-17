695199e344aa2805c1c797fb8299c26a6ab728ba
nullcast <masaki.kurosawa@lpconsulting.jp>
2022-07-17 18:53:07 +0900

    Fix brew script

--- .bin/init.sh
+++ .bin/init.sh
@@ -11,5 +11,8 @@ chsh -s /bin/bash
 # Install xcode
 xcode-select --install > /dev/null
 
+# Update software
+softwareupdate --install-rosetta --agree-to-license
+
 # Install brew
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh > /dev/null
